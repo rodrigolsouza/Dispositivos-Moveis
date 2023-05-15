eed75dca3ee418fa8abf0dc7a5e98ab4e9198365
Ana Paula Souza Silva <apss7@discente.ifpe.edu.br>
2023-05-15 18:12:00 -0300

    tela principal com fluxo alarme e minha farmacia

---
+++ app/src/main/java/com/br/medicalplanner/MinhaFarmacia.java
@@ -0,0 +1,14 @@
+package com.br.medicalplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MinhaFarmacia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minha_farmacia);
+    }
+}
