583a253dfeb3c287fd1a6a5b6592528270e9a3fa
rodrigolsouza <rodrigo.lopessouza@hotmail.com>
2023-05-16 20:45:23 -0300

    criação das classes dominio

--- app/src/main/java/com/br/medicalplanner/CadastroRemedio.java
+++ app/src/main/java/com/br/medicalplanner/CadastroRemedio.java
@@ -2,17 +2,36 @@ package com.br.medicalplanner;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastroRemedio extends AppCompatActivity {
+
+    private ButtonBarLayout botao_cadastrar_remedio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_remedio);
 
+        IniciarComponentes();
+
+        botao_cadastrar_remedio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CadastroRemedio.this, Principal.class);
+                startActivity(intent);
+            }
+        });
+
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void IniciarComponentes(){
+        botao_cadastrar_remedio=findViewById(R.id.botao_cadastrar_remedio);
     }
 }
---
+++ app/src/main/java/com/br/medicalplanner/dominio/Remedio.java
@@ -0,0 +1,64 @@
+package com.br.medicalplanner.dominio;
+
+public class Remedio {
+
+    private int id;
+
+    private  String nomeComercial;
+
+    private String principioAtivo;
+
+    private double dosagem;
+
+    private String formato;
+
+    private String validade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeComercial() {
+        return nomeComercial;
+    }
+
+    public void setNomeComercial(String nomeComercial) {
+        this.nomeComercial = nomeComercial;
+    }
+
+    public String getPrincipioAtivo() {
+        return principioAtivo;
+    }
+
+    public void setPrincipioAtivo(String principioAtivo) {
+        this.principioAtivo = principioAtivo;
+    }
+
+    public double getDosagem() {
+        return dosagem;
+    }
+
+    public void setDosagem(double dosagem) {
+        this.dosagem = dosagem;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+}
